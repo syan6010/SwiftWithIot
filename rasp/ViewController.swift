e6141e342aae8d036221ebf501417f63d8b288f5
shinngenn6010 <nogizaka46top@gmail.com>
2018-10-01 11:49:32 +0800

    Initial Commit

---
+++ rasp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rasp
+//
+//  Created by benny on 10/1/30 H.
+//  Copyright © 30 Heisei benny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
